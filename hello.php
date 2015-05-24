ee41a2554ea3ba875807f188953a3963c63a84dd
GrilloPress <grillopress@gmail.com>
2015-05-24 19:28:15 +0000

    add basic php 101 file

---
+++ hello.php
@@ -0,0 +1,29 @@
+<?php
+
+  echo "hi";
+
+?>
+
+<?php 
+
+  $name = "Andrew";
+  echo $name;
+
+?>
+
+<?php 
+
+  $thisMonth = date('M');
+  echo $thisMonth;
+
+?>
+
+<?php 
+
+  function say_hello() {
+    return "hello!";
+  }
+
+  echo say_hello();
+
+?>
